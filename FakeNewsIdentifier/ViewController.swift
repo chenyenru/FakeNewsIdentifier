71a13785eac223afec71642fd81748e841547c5e
ShazamR <42967019+ShazamR@users.noreply.github.com>
2021-01-16 10:47:47 +0800

    Initial Commit

---
+++ FakeNewsIdentifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeNewsIdentifier
+//
+//  Created by 陳姸汝 on 2021/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
